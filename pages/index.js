fb4f2e04bd96a235baa26c7e055dcc2fb4a5abf5
wolvdesign <56272249+wolvdesign@users.noreply.github.com>
2020-12-22 11:21:57 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,46 +5,45 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Sitio Ide Camping</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Sitio Ide
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Camping . Casamentos . Pesca{' '}
         </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Camping &rarr;</h3>
+            <p>Quer acampar no campo? Click e conheça!.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>Casamentos &rarr;</h3>
+            <p>Um espaço para casamentos pensado nos minimos detalhes!</p>
           </a>
 
           <a
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>Pesca &rarr;</h3>
+            <p>Torneios e Pesca esportiva o ano inteiro.</p>
           </a>
 
           <a
             href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h3>Deploy &rarr;</h3>
+            <h3>Reservas &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              Preencha seus dados e entramos em contato logo em seguida.
             </p>
           </a>
         </div>
@@ -56,8 +55,8 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Criado Por{' '}
+          <img src="/wolves.svg" alt="Wolves Media" className={styles.logo} />
         </a>
       </footer>
     </div>
